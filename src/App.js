0844d612fe95f54f2ad5833b1e5f7c6467aa9bb1
luizinhosc <luizinhoanselmo@gmail.com>
2021-04-19 14:59:12 -0300

    .

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Container, Typography, Button, Grid } from "@material-ui/core";
+import imagemInicial from "./images/front.pt-br.png";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    root: {
+      flexGrow: 1
+    },
+    paper: {
+      padding: theme.spacing(2),
+      textAlign: "center",
+      color: theme.palette.text.secondary,
+    },
+  },
+}));
 
 function App() {
+  const classes = useStyles();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={classes.root}>
+      <Grid container spacing={0} >
+        <Grid item xs={12}>
+          <Container maxWidth="sm">
+            <Typography variant="h2" align="center">
+              Fidelize Fácil
+            </Typography>
+          </Container>
+        </Grid>
+        <Grid container spacing={8} >
+          <Grid item xs={12} align="center">
+            <img src={imagemInicial} alt="imagem inicial" />
+          </Grid>
+          <Grid item xs={6} align="right">
+            <Button size="large" variant="contained" color="secondary">
+              Entrar
+            </Button>
+          </Grid>
+          <Grid item xs={6} align="left">
+            <Button size="large" variant="outlined" color="secondary">
+              Cadastrar
+            </Button>
+          </Grid>
+        </Grid>
+      </Grid>
     </div>
   );
 }
